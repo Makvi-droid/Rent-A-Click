14a8ff27d7029e9738e3d1bb464ba255c1818b7a
Makvi-droid <pasbenauro05@gmail.com>
2025-08-12 13:48:10 +0800

    added log in and sign up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import LandingPage from "./pages/LandingPage"
+import Auth from "./pages/Auth"
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   
 
   return (
     <>
-     <LandingPage/>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+    
     </>
   )
 }
